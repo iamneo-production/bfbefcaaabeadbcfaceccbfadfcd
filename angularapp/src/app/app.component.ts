b2634b6873eafd1848a1c6e1f4528286150fd3d6
Iamneo <techadmin@iamneo.ai>
2023-07-31 14:54:32 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component,ElementRef, ViewChild  } from '@angular/core';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angularapp';
+  title = 'CurrencyApp';
+  amount :any;
+  result :any;
+
+  currencyExange :any = {
+    USD: 1.126735,
+    GBP: 0.876893,
+    INR: 79.6770
+}
+
+  @ViewChild('fromCurrency') scy!: ElementRef;
+  @ViewChild('toCurrency') toCurrency!: ElementRef;
+
+  currencyList: any = ['USD', 'GBP', 'INR'] 
+  onClickMe() {
+    if(this.convertFrom == "")
+    return;
+    if(this.convertTo == "")
+    return;
+   
+    let resultTest = this.convert(this.amount, this.convertFrom, this.convertTo);
+    resultTest = parseFloat(resultTest.toFixed(2)); // result to 2 decimal places
+      this.result = `${this.amount} ${this.convertFrom} = ${resultTest} ${this.convertTo}`;
+  }
+  
+
+  convert(amount: number, from: string, to: string): number {
+    let fromConversionCoefficient: number =this.getConversionCoefficient(from);
+
+    let toConversionCoefficient: number = this.getConversionCoefficient(to);
+
+    return (toConversionCoefficient / fromConversionCoefficient) * amount;
+  }
+ 
+  getConversionCoefficient(cur: string){
+   let ret=0;
+    switch(cur){
+      case "USD": ret =this.currencyExange.USD;
+      break;
+      case "GBP": ret =this.currencyExange.GBP;
+      break;
+      case "INR": ret =this.currencyExange.INR;
+      break;
+    }
+
+    return ret;
+  }
+	convertFrom = '';
+	onSourceSelected():void {
+		this.convertFrom = this.scy.nativeElement.value;
+	}
+
+  convertTo = '';
+	onTargetSelected():void {
+		this.convertTo = this.toCurrency.nativeElement.value;
+	}
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,18 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
