c8ba9282d8f82b0c0dc571df6a7bec2e57bedcea
Iamneo <techadmin@iamneo.ai>
2023-08-03 13:14:54 +0000

    Backup Commit

--- angularapp/e2e/protractor.conf.js
+++ angularapp/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://8081-bfbefcaaabeadbcfaceccbfadfcd.project.examly.io',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
